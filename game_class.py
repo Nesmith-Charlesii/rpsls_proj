dd60d62c1bae1a5725fb6d6d41cb68518dcd7dd4
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-20 11:51:16 -0400

    Created a run_game method in the Game class that will run the game_intro, play, and display_winner methods 'while' run_game executes

--- game_class.py
+++ game_class.py
@@ -5,3 +5,17 @@ class Game:
     def __init__(self):
         self.player_one = Player()
         self.player_two = Player()
+
+    def run_game(self):
+        self.game_intro()
+        self.play()
+        self.display_winner()
+
+    def game_intro(self):
+        pass
+
+    def play(self):
+        pass
+
+    def display_winner(self):
+        pass
