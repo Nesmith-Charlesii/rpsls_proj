4293899423044ab4d16182c583b9357e2298e40c
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-20 11:37:38 -0400

    Created the Human and Computer classes to inherit from the Player class

---
+++ computer_class.py
@@ -0,0 +1,7 @@
+from player_class import Player
+
+
+# inherit from Payer class uses 'Player' as class param and the super()
+class Computer(Player):
+    def __init__(self):
+        super().__init__()
