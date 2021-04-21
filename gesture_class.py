c288b914aeeb59b41af7d23220153d01dbe3b91f
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-21 17:18:02 -0400

    created a while loop in the throw_hand method of the human class that will re-prompt the user if the value entered is not a number or is not within the gesture list

---
+++ gesture_class.py
@@ -0,0 +1,4 @@
+my_obj = {'rock': ['paper', 'spock'],
+          'paper': ['lizard', 'rock']
+          }
+print(f"{my_obj['rock'].__contains__('paper')}")
--- human_class.py
+++ human_class.py
@@ -16,6 +16,30 @@ class Human(Player):
 
     def throw_hand(self):
         user_choice = input("\nChoose your hand! : ")
-        choice_index = int(user_choice)
-        self.chosen_gesture = self.gestures[choice_index]
-        print(f"\n{self.name} has chosen to play {self.chosen_gesture}")
+        # cast string input return to an integer
+        # choice_index = int(user_choice)
+        # list of integers for each index in the gesture list
+        index_array = [0, 1, 2, 3, 4]
+        while True:
+            if user_choice.isnumeric():
+                choice_index = int(user_choice)
+                if index_array.__contains__(choice_index):
+                    self.chosen_gesture = self.gestures[choice_index]
+                    print(f"\n{self.name} has chosen to play {self.chosen_gesture}")
+                    break
+                else:
+                    print("\nPlease choose from the list of gesture options")
+                    user_choice = input("\nChoose your hand! : ")
+            else :
+                print("\nPlease choose from the list of gesture options")
+                user_choice = input("\nChoose your hand! : ")
+        # user_choice = input("\nChoose your hand! : ")
+        # choice_index = int(user_choice)
+        # while choice_index != :
+        #     print("\nPlease choose from the list of gesture options")
+        #     user_choice = input("\nChoose your hand! : ")
+        #     choice_index = int(user_choice)
+        # else:
+        #     # choice_index = int(user_choice)
+        #     self.chosen_gesture = self.gestures[choice_index]
+        #     print(f"\n{self.name} has chosen to play {self.chosen_gesture}")
